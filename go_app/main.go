cbc715d1ecf039d9d1cb110ed176308d3eb8b4c8
Mumtoz Kodirov <imumtoz@gmail.com>
2025-01-25 08:49:51 +0300

    .

--- go_app/main.go
+++ go_app/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"runtime"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte("Hello!"))
+func main() {
+	// Установить максимальное количество потоков для использования
+	numCPU := runtime.NumCPU()
+	runtime.GOMAXPROCS(numCPU)
+	fmt.Printf("Using %d CPU cores\n", numCPU)
+
+	// Обработчик для корневого маршрута
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("Hello!"))
+	})
+
+	// Создание сервера
+	server := &http.Server{
+		Addr:    "localhost:8000",
+		Handler: nil, // Используем глобальный HTTP-маршрутизатор
+	}
+
+	// Запуск сервера
+	fmt.Println("Server is running on http://localhost:8000")
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("Server failed: %s\n", err)
+	}
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
-}
