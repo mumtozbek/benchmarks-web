df9ba884f777797ca1369b3a532f9402e4d1420b
Mumtoz Kodirov <imumtoz@gmail.com>
2025-02-12 15:41:02 +0300

    zig updated

--- zig_app/server.zig
+++ zig_app/server.zig
@@ -1,25 +1,20 @@
 const std = @import("std");
-const net = std.net;
-const Thread = std.Thread;
-
-const RESPONSE = "HTTP/1.1 200 OK\r\nContent-Length: 6\r\nContent-Type: text/plain\r\nConnection: close\r\n\r\nHello!";
-
-fn handle_client(client: net.Stream) void {
-    defer client.close();
-    var buf: [1024]u8 = undefined;
-    _ = client.read(&buf) catch return; // Читаем, но не обрабатываем запрос
-    _ = client.writeAll(RESPONSE) catch return;
-}
 
 pub fn main() !void {
-    var gpa = std.heap.page_allocator;
-    var server = try net.StreamServer.init(.{});
+    const address = try std.net.Address.parseIp4("0.0.0.0", 8000);
+
+    var server = try address.listen(std.net.Address.ListenOptions{});
     defer server.deinit();
 
-    try server.listen(net.Address.parseIp("0.0.0.0", 8000) catch unreachable);
-
     while (true) {
-        var client = server.accept() catch continue;
-        _ = Thread.spawn(.{}, handle_client, .{client}) catch continue;
+        try handleConnection(try server.accept());
     }
+}
+
+fn handleConnection(conn: std.net.Server.Connection) !void {
+    defer conn.stream.close();
+    var buffer: [1024]u8 = undefined;
+    var http_server = std.http.Server.init(conn, &buffer);
+    var req = try http_server.receiveHead();
+    try req.respond("Hello!", std.http.Server.Request.RespondOptions{});
 }
